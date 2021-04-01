a23c68a07f8258433c593d1e99fa6a21fd53811d
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2021-04-01 11:12:58 +0200

    Fixed windows location handling, closes #13

--- src/main/java/com/daimler/sechub/model/FileLocationExplorer.java
+++ src/main/java/com/daimler/sechub/model/FileLocationExplorer.java
@@ -2,6 +2,7 @@
 package com.daimler.sechub.model;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,10 +10,14 @@ import java.nio.file.PathMatcher;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileLocationExplorer {
+
+	private static final Pattern PATTERN_ALL_SLASHES=Pattern.compile("\\/");
 
 	private List<Path> searchFolders = new ArrayList<>();
 
@@ -22,7 +27,7 @@ public class FileLocationExplorer {
 
 	/**
 	 * Searches for given location string
-	 * 
+	 *
 	 * @param location represents known location to search for inside defined search folders
 	 * @return list of matching files, never <code>null</code>
 	 * @throws IOException
@@ -30,15 +35,39 @@ public class FileLocationExplorer {
 	public List<Path> searchFor(String location) throws IOException {
 		List<Path> result = new ArrayList<>();
 
+		FileSystem defaultFileSystem = FileSystems.getDefault();
+		String separator = defaultFileSystem.getSeparator();
+
+		String osSpecificPathLocationRegexp = convertLocationOSSpecificRegExp(location, separator);
+
+		PathMatcher matcher = defaultFileSystem.getPathMatcher("regex:.*" + osSpecificPathLocationRegexp);
+
 		for (Path searchFolder : searchFolders) {
-			searchFilesRecursive(location, searchFolder,result);
+			searchFilesRecursive(matcher, searchFolder,result);
 		}
 
 		return result;
 	}
 
-	private void searchFilesRecursive(String location, Path searchFolder, List<Path> result) throws IOException {
-		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("regex:.*" + location);
+	/**
+	 * Converts given location to OS specific location java regexp. E.g. when seperator is '\' (windows), a location
+	 * like 'src/main/java/Test1.java' will be transformed to 'src\\main\\java\\Test1.java'.
+	 * @param location
+	 * @param separator
+	 * @return OS specific location
+	 */
+	String convertLocationOSSpecificRegExp(String location, String separator) {
+		if ("\\".equals(separator) ) {
+			Matcher matcher = PATTERN_ALL_SLASHES.matcher(location);
+			if (matcher.find()) {
+				return matcher.replaceAll("\\\\\\\\");
+			}
+		}
+		return location;
+	}
+
+	void searchFilesRecursive(PathMatcher matcher, Path searchFolder, List<Path> result) throws IOException {
+
 		Collection<Path> found = find(searchFolder, matcher);
 		for (Path path: found) {
 			result.add(path);
